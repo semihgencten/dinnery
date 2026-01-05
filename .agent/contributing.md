7047067ecb6e40191416db6b4ea16f0e8dadb69c
semihgencten <semih.gencten@udemy.com>
2026-01-05 20:49:44 +0300

    feat: add ingredients module

---
+++ .agent/contributing.md
@@ -0,0 +1,11 @@
+## TDD
+this projects follows TDD approach
+you can run tests with `pnpm test`
+
+We especially give value to end to end tests. New features should be implemented after 
+end to end tests are implemented. End to end test should not look into implementation details. It should look for the behaviour for users.
+
+Other tests should be implemented after the implementation of code.
+In brief when we develop a new feature we should follow: 
+end to end test -> implementation -> unit tests
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { RecipesController } from './recipes/recipes.controller';
 import { RecipesService } from './recipes/recipes.service';
 import { RecipesModule } from './recipes/recipes.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
 
 @Module({
-  imports: [RecipesModule],
-  controllers: [AppController, RecipesController],
-  providers: [AppService, RecipesService],
+  imports: [RecipesModule, IngredientsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
